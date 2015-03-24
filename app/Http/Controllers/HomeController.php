a38ed9c8275a388a74c53cf070ee254fc77b36ee
MAMISHO <mamisho@hotmail.es>
2015-03-24 16:32:38 +0100

    DB Selected from test
    Esqueloto del query join de la BD

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,6 +33,9 @@ class HomeController extends Controller {
 	public function index()
 	{
 		if(\Session::get('miSession')){
+			$examenes = $this->getExamenes();
+			dd($examenes);
+
 			return view('home');	
 		}
 		return redirect($this->loginPath());
@@ -44,5 +47,42 @@ class HomeController extends Controller {
 		// return property_exists($this, 'loginPath') ? $this->loginPath : '/auth/login';
 		return property_exists($this, 'loginPath') ? $this->loginPath : '/';
 	}
+
+	private function getExamenes(){
+		$user_data = $this->getUserData();
+
+		// $test = \DB::table('examenes')
+		// 	->where('usuario', \Session::get('miSession', 'usuario'))
+		// 	->get();
+
+		$test = \DB::table('usuarios_examenes')
+			->join('examenes', 'usuarios_examenes.examen_id', '=', 'examenes.id')
+            ->join('usuarios', 'usuarios_examenes.usuario_id', '=', 'usuarios.id')
+            ->where('usuarios.id',$user_data['id'])
+            ->groupBy('usuarios_examenes.id')
+            ->get();
+
+			$examen = null;
+			$cont =0;
+			foreach($test as &$aux) {
+				$examen[$cont]    = get_object_vars($aux);
+				$cont++;
+			}
+
+		return $examen;
+	}
+
+	private function getUserData(){
+
+			$user = \DB::table('usuarios')
+			->where('usuario', \Session::get('miSession', 'usuario'))
+			->get();
+
+			$usuario = null;
+			foreach($user as &$aux) {
+				$usuario     = get_object_vars($aux);
+			}
+			return $usuario;
+		}
 
 }
