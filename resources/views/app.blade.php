f6fd4d21257f8d29330aa582ac53747eb31eb1ef
MAMISHO <mamisho@hotmail.es>
2015-03-30 14:53:37 +0200

    Responsive 0.9
    App responsive

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,39 +25,20 @@
 
 </head>
 <body>
-	<nav class="onyx onyx-toolbar onyx-light">
+	<nav class="onyx onyx-toolbar onyx-light topper">
 		<div class="container-fluid">
 			<div class="navbar-header">
-				<!-- <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button> -->
 				<a class="navbar-brand titulo" style="text-decoration: underline;" href="{{ url('/home') }}">Exámenes</a>
 			</div>
-
-			<!-- <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"> -->
-				<!-- <ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}">Home</a></li>
-				</ul>
- -->
-				<ul class="nav navbar-nav navbar-right">
+				<ul class="nav navbar-nav navbar-right" id="nav_principal">
 					@if (!Session::get('miSession'))
 						<li><a href="{{ url('/') }}">Login</a></li>
-						<!-- <li><a href="{{ url('/auth/register') }}">Register</a></li> -->
 					@else
-						<!-- <nav>
-				            <ul class="fancyNav"> -->
 				            @if (Session::get('tipo_usuario')=="administrador")
 				            	<li id="op1"><a href="{{ url('/crear/examen') }}" class="homeIcon"><i class="fa fa-plus-circle"></i> Crear examen</a></li>
 				            @else
 				                <li id="op1"><a href="{{ url('/home') }}" class="homeIcon"><i class="fa fa-list-alt"></i> Nuevo examen</a></li>
 				            @endif
-				             <!--    <li id="op2"><a href="#news">Opciones</a></li>
-				                <li id="op3"><a href="#about">About us</a></li>
-				                <li id="op4"><a href="#services">Services</a></li>
-				                <li id="op5"><a href="#contact">Tabla de claves</a></li> -->
 				                <li class="dropdown">
 									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-key"></i>Trajeta de claves <span class="caret"></span></a>
 									<ul class="dropdown-menu" role="menu">
@@ -65,8 +46,6 @@
 										<li><a href="{{ url('/tablaClaves/descargar') }}"><i class="fa fa-file-pdf-o"></i> &nbsp;Descargar</a></li>
 									</ul>
 								</li>
-				     <!--        </ul>
-				        </nav> -->
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="bienvenida"><i class="fa fa-user"></i> Bienvenido/a</span> {{ Session::get('nombre') }}&nbsp;{{ Session::get('apellidos') }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
@@ -75,9 +54,35 @@
 						</li>
 					@endif
 				</ul>
+				<ul id="nav_mobile">
+					<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="bienvenida"><i class="fa fa-user"></i> Bienvenido/a</span> {{ Session::get('nombre') }}<span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="{{ url('/auth/logout') }}"><i class="fa fa-power-off"></i> &nbsp;Salir</a></li>
+								@if (!Session::get('miSession'))
+									<li><a href="{{ url('/') }}">Login</a></li>
+								@else
+							            @if (Session::get('tipo_usuario')=="administrador")
+							            	<li id="op1"><a href="{{ url('/crear/examen') }}" class="homeIcon"><i class="fa fa-plus-circle"></i> Crear examen</a></li>
+							            @else
+							                <li id="op1"><a href="{{ url('/home') }}" class="homeIcon"><i class="fa fa-list-alt"></i> Nuevo examen</a></li>
+							            @endif
+							                <!-- <li class="dropdown"> -->
+												<!-- <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-key"></i>Trajeta de claves <span class="caret"></span></a> -->
+												<!-- <ul class="dropdown-menu" role="menu"> -->
+													<li><a href="{{ url('/tablaClaves/ver') }}"><i class="fa fa-file-pdf-o"></i> &nbsp;Ver</a></li>
+													<li><a href="{{ url('/tablaClaves/descargar') }}"><i class="fa fa-file-pdf-o"></i> &nbsp;Descargar</a></li>
+												<!-- </ul> -->
+											<!-- </li> -->
+								@endif
+							</ul>
+						</li>
+				</ul>
 			<!-- </div> -->
 		</div>
 	</nav>
+	<br />
+	<br />
 
 	@yield('content')
 
